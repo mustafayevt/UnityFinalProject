9923e7a3ae090a02d006a6545476b7e06d270634
mustafayevt <mustafayevtural46@gmail.com>
2020-07-09 19:55:32 +0400

    Initial commit

---
+++ Assets/Scripts/General/AccelerateTowards.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateTowards : MonoBehaviour
+{
+    [SerializeField]
+    Transform transTowards;
+
+    [SerializeField]
+    float fSpeed;
+
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        if (transTowards == null)
+        {
+            transTowards = FindObjectOfType<AddPlayerControlledVelocity>().transform;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 v3MeTowardsTarget = transTowards.position - transform.position;
+        rigid.velocity += v3MeTowardsTarget.normalized * fSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/General/DestroyOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    [SerializeField]
+    bool bDestroySelf = false;
+
+    [SerializeField]
+    bool bDestroyOther = false;
+
+    public Text scoreBoard;
+    private int score = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+        {
+            if (bDestroySelf)
+                Destroy(this.gameObject);
+            if (bDestroyOther)
+            {
+                Destroy(collision.gameObject);
+                scoreBoard.text = (score+=1).ToString();
+            }
+        }
+    }
+}
